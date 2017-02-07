44c7cd713765e257051585b38dd1ce551a681b0e
Lera Selyuzhenok <lselyuzh@dmawi.de>
2017-02-07 14:04:09 +0300

    lera

--- Kara_SIC_Mean_Nov_Mar.py
+++ Kara_SIC_Mean_Nov_Mar.py
@@ -20,24 +20,24 @@ yE,mE,eE,cE = np.loadtxt('EKara_SIC_monthly_1978-2015.txt',skiprows=1,unpack=Tru
 dates = [datetime.date(y[i],m[i],1) for i in range(len(y))]
 
 years = np.arange(1978,2016)
-months_y2 = np.arange(1,4)
-month
 
 data = []
 dataW = []
 dataE = []
 
+for i in range(len(y)):
+    for j in range(len(m)):
+        if m[i]<6:
+            m[i]=m[i]+12
+            y[i]=y[i]-1
+print y, m            
 for i in range(len(years)):
     data_y = []
     dataW_y = []
     dataE_y = []
-    for j in range(len(y)):
-        if (y == years[i])&(m[j]<4):
-            
-        
-    ind_y1 = np.where(y==years[i])
-    ind_y2 = np.where(y==years[i+1])
-    
+      
+    ind_y = np.where(y==years[i])
+   
     mean_e = e[ind_y].mean()
     mean_c = c[ind_y].mean()
     
@@ -58,52 +58,52 @@ np.savetxt('WKara_SIC_Nov_Mar_1978-2015.txt',dataE,fmt='%1.0f')
 np.savetxt('EKara_SIC_Nov_Mar_1978-2015.txt',dataE,fmt='%1.0f')
 np.savetxt('Kara_SIC_Nov_Mar_1978-2015.txt',data,fmt='%1.0f')
     
-##plot extent
-#fig, ax = plt.subplots()
-#ax.set_title('Sea Ice Extent')
-#ax.plot(dates, e, 'r-')
-#ax.plot(dates, e, 'ro', label = 'Kara Sea')
-#
-#ax.plot(dates, eW, 'b-')
-#ax.plot(dates, eW, 'bo', label = 'Western Kara Sea')
-#
-#ax.plot(dates, eE, 'g-')
-#ax.plot(dates, eE, 'go', label = 'Eastern Kara Sea')
-#
-#ax.set_ylabel(u"km^2$")
+#plot extent
+fig, ax = plt.subplots()
+ax.set_title('Sea Ice Extent')
+ax.plot(years, data[:,1], 'r-')
+ax.plot(years, data[:,1], 'ro', label = 'Kara Sea')
+
+ax.plot(years, dataW[:,1], 'b-')
+ax.plot(years, dataW[:,1], 'bo', label = 'Western Kara Sea')
+
+ax.plot(years, dataE[:,1], 'g-')
+ax.plot(years, dataE[:,1], 'go', label = 'Eastern Kara Sea')
+
+ax.set_ylabel(u"km^2$")
 ## format the ticks
 #ax.xaxis.set_major_locator(years)
 #ax.xaxis.set_major_formatter(yearsFmt)
 #ax.xaxis.set_minor_locator(months)
-#
+
 #fig.autofmt_xdate()
-#plt.grid()
-#plt.legend()
-#plt.show()
-#plt.savefig('Kara_SIE')
-#
-#
-##plot concentration
-#fig, ax = plt.subplots()
-#ax.set_title('Sea Ice Concentration')
-#ax.plot(dates, c, 'r-')
-#ax.plot(dates, c, 'ro', label = 'Kara Sea')
-#
-#ax.plot(dates, cW, 'b-')
-#ax.plot(dates, cW, 'bo', label = 'Western Kara Sea')
-#
-#ax.plot(dates, cE, 'g-')
-#ax.plot(dates, cE, 'go', label = 'Eastern Kara Sea')
-#
-#ax.set_ylabel(u"%")
-## format the ticks
+plt.grid()
+plt.legend()
+plt.show()
+plt.savefig('Kara_SIE')
+
+
+#plot concentration
+fig, ax = plt.subplots()
+ax.set_title('Sea Ice Concentration')
+ax.plot(years, data[:,2], 'r-')
+ax.plot(years, data[:,2], 'ro', label = 'Kara Sea')
+
+ax.plot(years, dataW[:,2], 'b-')
+ax.plot(years, dataW[:,2], 'bo', label = 'Western Kara Sea')
+
+ax.plot(years, dataE[:,2], 'g-')
+ax.plot(years, dataE[:,2], 'go', label = 'Eastern Kara Sea')
+
+ax.set_ylabel(u"%")
+# format the ticks
 #ax.xaxis.set_major_locator(years)
 #ax.xaxis.set_major_formatter(yearsFmt)
 #ax.xaxis.set_minor_locator(months)
-#
+
 #fig.autofmt_xdate()
-#plt.grid()
-#plt.legend()
-#plt.show()
-#
-#plt.savefig('Kara_SIC')
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.savefig('Kara_SIC')
